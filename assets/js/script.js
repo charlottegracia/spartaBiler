8eeeedd434e0b6b36e31263c56e31360901c8fee
charlottegracia <1086120@ucn.dk>
2021-12-22 11:27:33 +0100

    pages added

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+header();
+footer();
+
+function header() {
+    let text = `
+            <nav class="opensans">
+                <ul>
+                    <li>
+                        <a href="index.html">
+                            nav1
+                        </a>
+                    </li>
+                    <section class="navFlex">
+                        <li>
+                            <a href="leasing.html">Leasing</a>
+                        </li>
+                        <li>
+                            <a href="biler.html">Biler på lager</a>
+                        </li>
+                        <li>
+                            <a href="kontakt.html">Kontakt</a>
+                        </li>
+                    </section>
+                    <i id="navIkon2" class="fas fa-times" onclick="dropdownMobile()"></i>
+                    <i id="navIkon" class="fas fa-bars" onclick="dropdownMobile()"></i>
+                </ul>
+                <div class="nav-fade-in dropdownMobile">
+                    <ul id="navMobile">
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="leasing.html">Leasing</a>
+                        </li>
+                        <li>
+                            <a href="biler.html">Biler på lager</a>
+                        </li>
+                        <li>
+                            <a href="kontakt.html">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    `;
+    document.querySelector('header').innerHTML = text;
+}
+
+function dropdownMobile() {
+    if (document.getElementById("navMobile").style.display === "block") {
+        document.getElementById("navMobile").style.display = "none";
+        document.querySelector('main').style.display = "block";
+        document.querySelector('footer').style.display = "block";
+        document.querySelector('nav ul li a').style.display = "block";
+        document.getElementById("navIkon2").style.display = "none";
+        document.getElementById("navIkon").style.display = "block";
+        document.querySelector('.nav-fade-in').classList.remove('appear');
+
+    } else {
+        document.getElementById("navMobile").style.display = "block";
+        document.querySelector('main').style.display = "none";
+        document.querySelector('footer').style.display = "none";
+        document.querySelector('nav ul li a').style.display = "none";
+        document.getElementById("navIkon2").style.display = "block";
+        document.getElementById("navIkon").style.display = "none";
+        document.querySelector('.nav-fade-in').classList.add('appear');
+    }
+}
+
+function footer() {
+    let text = `
+    <h2>footer</h2>
+    `;
+    document.querySelector('footer').innerHTML = text;
+}
