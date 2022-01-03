efe13510631ef83e0d146c67377c57beaba6995c
charlottegracia <1086120@ucn.dk>
2022-01-03 16:45:44 +0100

    specific car done

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,7 +128,6 @@ function onScrollChecks() {
                 illustration2.style.transform="translateX("+ tmpX + "px)";
             }
         } */
-
 };
 
 function leasing(leasingtype) {
@@ -146,3 +145,30 @@ function leasing(leasingtype) {
         document.getElementById('splitleasing').style.display = "flex"
     }
 }
+
+/* KODE NEDENFOR ER FRA W3SCHOOLS. Kilde: https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_self */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    if (x.length > 0) {
+        x[slideIndex-1].style.display = "block";  
+    }
+}
+
+/* KODE OVENFOR ER FRA W3SCHOOLS. Kilde: https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_self */
